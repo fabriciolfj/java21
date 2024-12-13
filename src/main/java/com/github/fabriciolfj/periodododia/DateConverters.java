5bb52b6f6e6b033910b4a53696871b39dd6f1e4b
spark <fabricio.jacob@outlook.com>
2024-12-12 21:44:32 -0300

    add

---
+++ src/main/java/com/github/fabriciolfj/periodododia/DateConverters.java
@@ -0,0 +1,72 @@
+package com.github.fabriciolfj.periodododia;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Objects;
+
+public final class DateConverters {
+
+    private DateConverters() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static String toDayPeriodV1(Date date, ZoneId zoneId) {
+
+        Objects.requireNonNull(date, "The provided date cannot be null");
+        Objects.requireNonNull(zoneId, "The provided zone id cannot be null");
+
+        LocalTime lt = date.toInstant().atZone(zoneId).toLocalTime();
+       
+        LocalTime night = LocalTime.of(21, 0, 0);
+        LocalTime morning = LocalTime.of(6, 0, 0);
+        LocalTime afternoon = LocalTime.of(12, 0, 0);
+        LocalTime evening = LocalTime.of(18, 0, 0);
+        LocalTime almostMidnight = LocalTime.of(23, 59, 59);
+        LocalTime midnight = LocalTime.of(0, 0, 0);
+        
+        if((lt.isAfter(night) && lt.isBefore(almostMidnight)) 
+                || lt.isAfter(midnight) && (lt.isBefore(morning))) {
+            return "night";
+        } else if(lt.isAfter(morning) && lt.isBefore(afternoon)) {
+            return "morning";
+        } else if(lt.isAfter(afternoon) && lt.isBefore(evening)) {
+            return "afternoon";
+        } else if(lt.isAfter(evening) && lt.isBefore(night)) {
+            return "evening";
+        }
+
+        return "day";
+    }
+
+    public static String toDayPeriodV2(Date date, ZoneId zoneId) {
+
+        Objects.requireNonNull(date, "The provided date cannot be null");
+        Objects.requireNonNull(zoneId, "The provided zone id cannot be null");
+
+        ZonedDateTime zdt = date.toInstant().atZone(zoneId);
+
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy-MMM-dd'T'HH:mm:ss a Z");
+
+        System.out.println("Date-time to format as day period is: "
+                + zdt.withZoneSameInstant(zoneId).format(formatter1));
+
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MMM-dd [B]");
+
+        return zdt.withZoneSameInstant(zoneId).format(formatter2);
+    }
+    
+    public static void printToDayPeriod() {
+        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh B");
+        
+        for (int h = 0; h < 24; h++)
+        {
+            final OffsetDateTime odt
+                    = Instant.now().atOffset(ZoneOffset.UTC).withHour(h);
+            
+            System.out.println("Hour " + h 
+                    + ": \"" + formatter.format(odt) + "\"");
+        }
+    }
+}
---
+++ src/main/java/com/github/fabriciolfj/periodododia/Main.java
@@ -0,0 +1,61 @@
+package com.github.fabriciolfj.periodododia;
+
+import java.time.ZoneId;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       /* System.out.println(ZoneId.getAvailableZoneIds());
+
+        System.out.println();
+
+        // Africa/Johannesburg
+        System.out.println("Good "
+                + DateConverters.toDayPeriodV1(new Date(), ZoneId.of("Africa/Johannesburg"))
+                + ", Africa/Johannesburg");
+        System.out.println("Africa/Johannesburg: "
+                + DateConverters.toDayPeriodV2(new Date(), ZoneId.of("Africa/Johannesburg")));
+
+        System.out.println();
+
+        // Asia/Saigon
+        System.out.println("Good "
+                + DateConverters.toDayPeriodV1(new Date(), ZoneId.of("Asia/Saigon"))
+                + ", Asia/Saigon");
+        System.out.println("Asia/Saigon: "
+                + DateConverters.toDayPeriodV2(new Date(), ZoneId.of("Asia/Saigon")));
+
+        System.out.println();
+
+        // Australia/Melbourne
+        System.out.println("Good "
+                + DateConverters.toDayPeriodV1(new Date(), ZoneId.of("Australia/Melbourne"))
+                + ", Australia/Melbourne");
+        System.out.println("Australia/Melbourne: "
+                + DateConverters.toDayPeriodV2(new Date(), ZoneId.of("Australia/Melbourne")));
+
+        System.out.println();
+
+        // America/Resolute
+        System.out.println("Good "
+                + DateConverters.toDayPeriodV1(new Date(), ZoneId.of("America/Resolute"))
+                + ", America/Resolute");
+        System.out.println("America/Resolute: "
+                + DateConverters.toDayPeriodV2(new Date(), ZoneId.of("America/Resolute")));
+        
+        System.out.println();
+        
+        //  America/Argentina/Buenos_Aires
+        System.out.println("Good "
+                + DateConverters.toDayPeriodV1(new Date(), ZoneId.of("America/Argentina/Buenos_Aires"))
+                + ", America/Argentina/Buenos_Aires");
+        System.out.println("America/Argentina/Buenos_Aires: "
+                + DateConverters.toDayPeriodV2(new Date(), ZoneId.of("America/Argentina/Buenos_Aires")));*/
+        
+        // print hour-by-hour
+        System.out.println();
+        DateConverters.printToDayPeriod();
+    }
+}
